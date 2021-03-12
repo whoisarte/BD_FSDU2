f6c7fa024a022e86ee0384d32304f0d82f4f518b
Arte <arte.solo.arte@hotmail.com>
2021-03-12 15:26:13 -0600

    Último commit, espero...

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     nombre = models.TextField()
-    correo = models.TextField()
+    correo = models.TextField(default = "")
 
-    #def __str__(self):
-    #    return self.text[:50]
+    def __str__(self):
+    	return self.text[:50]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,10 @@
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView
 from .models import Post
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
     model = Post
-    context_object_name = 'nombre_correos'
+    context_object_name = 'superlista'
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'

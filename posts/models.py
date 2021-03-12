5beb0bc83769717bdd278451960275231fd8488f
Arte <arte.solo.arte@hotmail.com>
2021-03-12 01:41:50 -0600

    Intento de DB (no carga correctamente...)

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    nombre = models.TextField()
+    correo = models.TextField()
+
+    #def __str__(self):
+    #    return self.text[:50]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,10 @@
 from django.views.generic.base import TemplateView
+from .models import Post
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+    model = Post
+    context_object_name = 'nombre_correos'
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
